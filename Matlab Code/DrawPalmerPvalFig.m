95210e6ad26ee731a42ae00beb56000557bc264f
weinreichlab <lab.weinreich@gmail.com>
2017-07-18 13:06:45 -0400

    Modified to take advantage of the possibility that the relevant null distribution for tau_b may already be on the disk.
    (And if not, to save the distribution in the canonical place for use later.)

--- Matlab Code/DrawPalmerPvalFig.m
+++ Matlab Code/DrawPalmerPvalFig.m
@@ -12,45 +12,64 @@
 % June 23, 2017
 
 close all
+clear all
 
 % These first two values comes from TauTable.csv, which is produced by
 % BuildKendallTauTable.m
 ObsTaub = 0.1921;
 SizePalmerRanks = 55;
 genotypes = 64;
-
-total_pseudo_replicates = 1e5; 
-
-% Now build the canonical unitation for this dataset
-canonical_unitations = [];
 loci = log2(genotypes);
-for i=0:loci
-    for j=1:nchoosek(loci,i)
-        canonical_unitations(end+1) = i;
+
+% Now we need the null distribution of Tau_b values, which may already
+% exist on the disk... Here's the file name convention used in
+% BuildKendallTauTable.m
+NullTaubFilename = ...
+    sprintf('./NullTau_bDistrs/%dLoci%dSigWalsh.mat',loci,...
+    SizePalmerRanks);
+% First see if directory exists, and create it if necessary
+if ~exist('./NullTau_bDistrs')
+    mkdir('./NullTau_bDistrs');
+end
+% Now see if this particular null distribution has already been
+% simulated
+if exist(NullTaubFilename)
+    % if so, use it
+    load(NullTaubFilename)
+    total_pseudo_replicates = size(taub_null_distr,1);
+else
+    % Build the canonical unitation for this dataset
+    canonical_unitations = [];
+    for i=0:loci
+        for j=1:nchoosek(loci,i)
+            canonical_unitations(end+1) = i;
+        end
     end
+    % Now build the null distribution
+    total_pseudo_replicates = 1e5;
+    taub_null_distr = zeros(total_pseudo_replicates,1);
+    for i = 1:total_pseudo_replicates
+        test_sample = randsample(canonical_unitations, SizePalmerRanks);
+        test_sample2 = randsample(canonical_unitations, SizePalmerRanks);
+        taub_null_distr(i) = ktaub([test_sample' test_sample2'],0.05,0); 
+    end
+    taub_null_distr = sort(taub_null_distr);
+    % And save it for next time
+    save(NullTaubFilename,'taub_null_distr');
 end
-canonical_unitations = canonical_unitations;
 
-
-% Now build the null distribution and get P
-NullDistr = zeros(total_pseudo_replicates,1);
-for i = 1:total_pseudo_replicates
-    test_sample = randsample(canonical_unitations, SizePalmerRanks);
-    test_sample2 = randsample(canonical_unitations, SizePalmerRanks);
-    NullDistr(i) = ktaub([test_sample' test_sample2'],0.05,0); 
-end
-NullDistr = sort(NullDistr);
+% Finally we can get P
 for i= 1:total_pseudo_replicates
-    if ObsTaub < NullDistr(i)
+    if ObsTaub < taub_null_distr(i)
         break
     end
 end
-% not really: this is the integer index to plot with, rather than the (0,1)
-% probability itself.
+% not really: at this point pval is just integer index to plot with, 
+% rather than the (0,1) probability itself.
 pval1 = total_pseudo_replicates - i;
 
 % plot([1:-1/total_pseudo_replicates:1/total_pseudo_replicates],...
-plot([total_pseudo_replicates:-1:1],NullDistr,'--','LineWidth',2)
+plot([total_pseudo_replicates:-1:1],taub_null_distr,'--','LineWidth',2)
 hold on
 plot([pval1,pval1],[-.5,ObsTaub],'r','LineWidth',2)
 plot([0,pval1],[ObsTaub,ObsTaub],'r','LineWidth',2)
